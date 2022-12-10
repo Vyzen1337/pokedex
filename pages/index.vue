c0e501697ab5816b6b8a2149b02075cf67662b7b
Nikolai Wicke <n.wicke@flynet.de>
2022-12-10 22:16:33 +0100

    feat: add pages & base structure

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container mx-auto px-4">
+
+    <h1 class="text-gray-800 text-2xl lg:text-3xl font-bold my-12">
+      Pokédex
+    </h1>
+
+    <!-- <pre>{{ data }}</pre> -->
+    
+
+    <!-- <pre>
+      {{ data }}
+    </pre> -->
+
+
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-6">
+
+      <div v-for="(item) in data.results">
+
+        <div class="flex flex-col">
+          <span class="text-gray-500">
+            {{ item.name }}
+          </span>
+          <NuxtLink :to="`/pokemon/${item.name}`">
+            read more
+          </NuxtLink>
+
+          
+        </div>
+      </div>
+
+    </div>
+
+
+
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('https://pokeapi.co/api/v2/pokemon/')
+
+</script>
---
+++ pages/pokemon/[name].vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mx-auto px-4 py-12">
+    
+    <div class="grid md:grid-cols-2 gap-8">
+
+      <div class="space-y-4">
+        <div class="bg-gray-100 rounded-lg overflow-hidden relative">
+          <img :src="data.sprites.front_default" class="w-full h-full object-cover object-center" />
+        </div>
+      </div>
+
+      <div class="md:py-8">
+        <div class="mb-2 md:mb-3">
+          <span class="inline-block text-gray-500 mb-1">
+            #{{ data.id}}
+          </span>
+          <h1 class="text-gray-800 text-2xl lg:text-3xl font-bold">
+            {{ data.name }}
+          </h1>
+
+        </div>
+      </div>
+    </div>
+
+    <hr class="my-8"/>
+
+    <NuxtLink to="/">
+      Back
+    </NuxtLink>
+    
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const name = route.params.name
+
+const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+</script>
